a9bd0972437bb4f8d5293f319cbb0811509a8217
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-16 22:54:58 +0500

    2

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ import re
 pattern = r'ab*'
 
 
-test_strings = ["a", "ab", "abb", "ac", "b", "aab", ""]
+test_strings = ["a", "ab" ]
 
 for string in test_strings:
     if re.fullmatch(pattern, string):
